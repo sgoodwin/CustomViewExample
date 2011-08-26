18ffb459d0838be58bbc1b4c929ebfb727523beb
Samuel Goodwin <samuel.goodwin@me.com>
2011-08-26 12:10:31 -0400

    Initial Commit

---
+++ CustomViewExample/GOAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GOAppDelegate.h
+//  CustomViewExample
+//
+//  Created by Samuel Goodwin on 8/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GOViewController;
+
+@interface GOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GOViewController *viewController;
+
+@end
